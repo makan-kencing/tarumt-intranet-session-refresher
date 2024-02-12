bc8cd13a2289e62e7a653592e620c64543a7abc3
kongjiyu <133716765+kongjiyu@users.noreply.github.com>
2024-02-12 19:18:54 +0800

    Update index.js

--- popup/js/index.js
+++ popup/js/index.js
@@ -1,5 +1,5 @@
-const reset_button = document.querySelector("button[type=reset]");
-const submit_button = document.querySelector("button[type=submit]");
+const reset_button = document.querySelector("button#Cancle");
+const submit_button = document.querySelector("button#Save");
 
 reset_button.addEventListener("click", show_saved_value);
 submit_button.addEventListener("click", save_credentials);
