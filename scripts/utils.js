15fc782f8bfd774a286512c481645962932a5134
DizzyNight <dizzynight2096@gmail.com>
2024-02-15 13:15:29 +0800

    change to enum-like way of storing localstorage key

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,13 +1,18 @@
+const Storage = {
+    USERNAME: "tarIntranetUsername",
+    PASSWORD: "tarIntranetPassword"
+}
+
 async function get_local_username_password() {
-    const items = await chrome.storage.local.get(["tarIntranetUsername", "tarIntranetPassword"]);
-    return [items.tarIntranetUsername, items.tarIntranetPassword];
+    const items = await chrome.storage.local.get([Storage.USERNAME, Storage.PASSWORD]);
+    return [items[Storage.USERNAME], items[Storage.PASSWORD]];
 }
 
 function set_local_username_password(username, password) {
     chrome.storage.local.set(
         {
-            tarIntranetUsername: username,
-            tarIntranetPassword: password
+            [Storage.USERNAME]: username,
+            [Storage.PASSWORD]: password
         }
     );
 }
