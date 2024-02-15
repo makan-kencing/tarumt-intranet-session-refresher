1a7e24c8ed73e4cec7944fbec8889def489cd4c1
DizzyNight <dizzynight2096@gmail.com>
2024-02-15 18:35:47 +0800

    change cancel button to close window

--- popup/js/index.js
+++ popup/js/index.js
@@ -1,7 +1,7 @@
 const reset_button = document.querySelector("button#cancel");
 const submit_button = document.querySelector("button#save");
 
-reset_button.addEventListener("click", show_saved_value);
+reset_button.addEventListener("click", () => {window.close()});
 submit_button.addEventListener("click", () => {
     clear_input_box_red();
     clear_error();
