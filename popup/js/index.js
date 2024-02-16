a24a2081eca6e993a0a6eaed5c5d3bc91b29d80e
DizzyNight <dizzynight2096@gmail.com>
2024-02-16 22:59:20 +0800

    implement clear storage to ui

--- popup/js/index.js
+++ popup/js/index.js
@@ -1,6 +1,14 @@
+const clear_button = document.querySelector("#clear")
 const reset_button = document.querySelector("button#cancel");
 const submit_button = document.querySelector("button#save");
 
+clear_button.addEventListener("click", () => {
+    clear_storage();
+    clear_input_box_red();
+    clear_error();
+    clear_login_status();
+    show_saved_value();
+})
 reset_button.addEventListener("click", () => {window.close()});
 submit_button.addEventListener("click", () => {
     clear_input_box_red();
